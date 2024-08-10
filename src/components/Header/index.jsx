88d71e6c8ed0f9e05228b9e478f3851d12fb46ac
ayush050501 <ayushagrawal133.skb@gmail.com>
2024-08-10 21:06:27 +0530

    myweb

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
 import ToggleMode from '@/components/mode-toggle';
 import ThemeSwitcher from '@/components/themes';
+import { Separator } from '@/components/ui/separator';
 const Header = () => {
     return (
         <>
-            <div className="flex justify-between items-center h-full mx-4">
-                <Button>ayush</Button>
-                <div className="flex items-center h-full gap-2">
+            <div className="flex flex-col h-[60px]">
+                <div className="flex items-center h-full justify-end gap-2 mx-4">
                     <ToggleMode />
                     <ThemeSwitcher />
                 </div>
+                <Separator/>
             </div>
         </>
     );
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,4 +1,4 @@
-import { CardWithForm } from '../App/NewCard';
+import { CardWithForm } from '../pages/NewCard';
 import Header from '../components/Header';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 function Home() {
@@ -6,22 +6,19 @@ function Home() {
     return (
         <>
             <ResizablePanelGroup direction="horizontal" className={`rounded-lg border max-w-${width}}`}>
-                <ResizablePanel maxSize={15}>
+                <ResizablePanel maxSize={15} className="min-w-[60px]">
                     <div className="flex h-screen items-center justify-center p-6">
                         <span className="font-semibold">One</span>
                     </div>
                 </ResizablePanel>
                 <ResizableHandle />
                 <ResizablePanel>
-                    <ResizablePanelGroup direction="vertical">
-                        <ResizablePanel maxSize={8} minSize={8}>
-                            <Header/>
-                        </ResizablePanel>
-                        <ResizableHandle />
-                        <ResizablePanel className="p-6">
-                            <CardWithForm/>
-                        </ResizablePanel>
-                    </ResizablePanelGroup>
+                    <div className="min-h-[60px] max-h-[60px]">
+                        <Header />
+                    </div>
+                    <main className='p-6 max-h-[clac(100vh-60px)] overflow-y-auto' >
+                        <CardWithForm />
+                    </main>
                 </ResizablePanel>
             </ResizablePanelGroup>
         </>
